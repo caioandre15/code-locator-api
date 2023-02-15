a90334eac710e0221e2d481110528cc3f2b8f26d
CAIO ANDRE <caio.andre@juntossomosmais.com.br>
2023-02-14 22:34:31 -0300

    Configuring the EF.

---
+++ Locator/src/Locator.Data/Context/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locator.Data.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+    }
+}
--- Locator/src/Locator.api/Program.cs
+++ Locator/src/Locator.api/Program.cs
@@ -1,3 +1,6 @@
+using Locator.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataBaseContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

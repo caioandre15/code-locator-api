ed0972ab2101933c0229e9c0df8cd6e9505ab582
CAIO ANDRE <caio.andre@juntossomosmais.com.br>
2023-03-20 21:19:00 -0300

    Create unit test.

---
+++ Locator/src/Testes/Locator.Tests/Unit/CarServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locator.Tests.Unit
+{
+    public class CarServiceTests
+    {
+        [Fact]
+        public void GetListCars_ListCars_ReturnsAList()
+        {
+
+        }
+    }
+}
